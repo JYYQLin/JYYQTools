d5c514b1e68b3979172dd8e43c82c828cdb66580
JYYQLin <92315746+JYYQLin@users.noreply.github.com>
2024-10-13 18:06:55 +0800

    JYYQLin
    提交代码

---
+++ JYTools/JYTools/Tools/Extension/UIApplication + Extension.swift
@@ -0,0 +1,10 @@
+//
+//  UIApplication + Extension.swift
+//  JYTools
+//
+//  Created by 林君扬 on 2024/10/13.
+//
+
+import UIKit
+
+
---
+++ JYTools/JYTools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JYTools
+//
+//  Created by 林君扬 on 2024/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
